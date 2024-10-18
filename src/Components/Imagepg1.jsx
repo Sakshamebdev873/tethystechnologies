9e22e71b73833c3930a5649db730fbd21c39a8fa
Sakshamarya873 <vinodarya344@gmail.com>
2024-10-18 17:13:38 +0530

    first

--- src/Components/Imagepg1.jsx
+++ src/Components/Imagepg1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Imagepg = () => {
   return (
   <div className='flex items-center justify-center  relative ' > 
-  <img src="../src/assets/images cer.jpg" alt="phote" className='object-cover  ' />
+  <img src="/images cer.jpg" alt="phote" className='object-cover  ' />
   </div>
   )
 }
